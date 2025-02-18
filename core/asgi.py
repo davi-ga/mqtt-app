bf37bf918a46bf03bf17fba189eb30bad1cbaf1b
Davi-Ga <contato@daviga.dev.br>
2025-02-18 10:12:21 -0300

    Refactor ASGI configuration to support WebSocket connections with MQTT consumer

--- core/asgi.py
+++ core/asgi.py
@@ -1,16 +1,18 @@
-"""
-ASGI config for core project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
+# filepath: /home/daviga/projects/mqtt-app/mqtt_app/asgi.py
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from django.urls import path
+from features.subscribe.services.consumer import MqttConsumer
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mqtt_app.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter([
+            path("ws/mqtt/", MqttConsumer.as_asgi()),
+        ])
+    ),
+})
