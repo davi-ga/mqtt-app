b869a6962de4c2171126fdae760191059c3222d5
Davi-Ga <contato@daviga.dev.br>
2025-02-18 10:12:45 -0300

    Add MQTT consumer for handling WebSocket connections

---
+++ features/subscribe/services/consumer.py
@@ -0,0 +1,22 @@
+# filepath: /home/daviga/projects/mqtt-app/mqtt_app/consumers.py
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+class MqttConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.channel_layer.group_add("mqtt_group", self.channel_name)
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard("mqtt_group", self.channel_name)
+
+    async def receive(self, text_data):
+        pass
+
+    async def mqtt_message(self, event):
+        message = event['message']
+        topic = event['topic']
+        await self.send(text_data=json.dumps({
+            "topic": topic,
+            'message': message
+        }))
