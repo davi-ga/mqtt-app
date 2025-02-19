714a2bd77bca8aab6b1944379fc561e1930224c9
davi-ga <contato@daviga.dev.br>
2025-02-19 16:47:37 -0300

    Add WebSocket URL to context in subscribe_view for result rendering

--- features/subscribe/app/views.py
+++ features/subscribe/app/views.py
@@ -10,15 +10,18 @@ def subscribe_view(request):
         if form.is_valid():
             selected_options = form.cleaned_data["select_field"]
             name = form.cleaned_data["name"]
-            user_ip = request.META.get('REMOTE_ADDR')
+            user_ip = request.META.get("REMOTE_ADDR")
             client = MqttClient(
                 broker_ip=settings.MQTT_BROKER_HOST,
-                client_name=name+user_ip,
+                client_name=name + user_ip,
                 topics=selected_options,
             )
             client.start_connection()
+            ws_url = settings.WS_URL
             return render(
-                request, "result.html", context={"selected_options": selected_options}
+                request,
+                "result.html",
+                context={"selected_options": selected_options, "ws_url": ws_url},
             )
     else:
         form = SubscribeForm()
